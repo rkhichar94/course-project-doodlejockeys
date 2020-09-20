fa15c741274dae467c2c843146f2894736c04cd3
claracoo <claracoo@buffalo.edu>
2020-09-20 11:58:15 -0400

    starting skeleton

---
+++ doodle.bob/src/components/Chat.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// class Chat extends Component {
+//     state = {  }
+//     render() { 
+//         return (  );
+//     }
+// }
+
+export default Chat;
