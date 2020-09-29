479a19ce96f6d16a318f77647083dc2a88c2f0bf
Victoria Dib <vdib99@gmail.com>
2020-09-29 00:31:48 -0400

    User Story #6 Made buttons within toolbar easier to read.

--- doodle.bob/src/components/Toolbar.jsx
+++ doodle.bob/src/components/Toolbar.jsx
@@ -12,28 +12,27 @@ class Toolbar extends Component {
     
       handleClick() {
         this.setState(prevState => ({
-          isToggleOn: !prevState.isToggleOn
+          isToggleOn: !prevState.isToggleOn,
         }));
       }
-    
+
       render() {
         return (
             <div className = "toolbar">
-                
                     <button onClick={this.handleClick}> {this.state.isToggleOn ? 'Show Toolbar' : 'Hide Toolbar'}</button>
-                    <button>Change brush</button>
+                    <button>Pencil</button>
+                    <button>Eraser</button>
                     <button>Change brush width</button>
                     <button style = {{backgroundColor:"white"}}>1</button>
-                    <button style = {{backgroundColor:"black"}}>2</button>
+                    <button style = {{backgroundColor:"black",color:"white"}}>2</button>
                     <button style = {{backgroundColor:"red"}}>3</button>
                     <button style = {{backgroundColor:"orange"}}>4</button>
                     <button style = {{backgroundColor:"yellow"}}>5</button>
                     <button style = {{backgroundColor:"green"}}>6</button>
                     <button style = {{backgroundColor:"blue"}}>7</button>
-                    <button style = {{backgroundColor:"indigo"}}>8</button>
+                    <button style = {{backgroundColor:"indigo", color: "white"}}>8</button>
                     <button style = {{backgroundColor:"violet"}}>9</button>
                     <button>Clear board</button>
-                    <button>Eraser</button>
             </div>
         );
       }
