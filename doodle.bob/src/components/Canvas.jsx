f62cc55d07a294525e57e96984b4e820a16fd771
Victoria Dib <vdib99@gmail.com>
2020-09-30 13:12:43 -0400

    User  story #6 Erase All button functioanlity added

--- doodle.bob/src/components/Canvas.jsx
+++ doodle.bob/src/components/Canvas.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Sketch from 'react-p5';
 import "../styles.css";
 
+
 class Stroke {
 	constructor(_init) {
 		this.init = _init;
@@ -27,37 +28,61 @@ class Canvas extends Component {
 		super(props);
 		this.state = {
 			lastStrokeIdx: -1,
-			erasing: false
+			erasing: false,
 		}
-    }
+	}
 
+	setup = (p5, parent) => {
+		p5.createCanvas(700,500).parent(parent)
+		p5.background(255);
+
+		var eraserbtn = p5.createButton("Erase All");
+		eraserbtn.mousePressed(this.resetSketch);
+		// eraserbtn.position();
+		
+
+	}
+
+	resetSketch = () => {
+		this.setState({
+			erasing:true
+		})
+	}
+
+	draw = (p5) => {
+		p5.background(255);
+		p5.noLoop();
+	}
+
+	mousePressed = (p5) => {
+		if(this.state.erasing){
+			p5.background(255);
+			this.setState({erasing: false});
+		}
+		else{
+			this.setState({
+				lastStrokeIdx: this.state.lastStrokeIdx + 1
+			});
+			ALL_STROKES.push(new Stroke(p5.createVector(p5.mouseX, p5.mouseY)));
+		}
+	}
+
+	mouseDragged =  (p5) => {
+			ALL_STROKES[this.state.lastStrokeIdx].add(p5, p5.createVector(p5.mouseX, p5.mouseY));
+	}
+
+	
     render(props) {
         return (
 		<div id="canvas">
 			<Sketch 
-				setup={(p5, parent) => {
-					p5.createCanvas(700, 500).parent(parent);
-				}}
-				
-				draw = {(p5) => {
-					p5.background(255);
-					p5.noLoop();
-				}}
-
-				mousePressed = {p5 => {
-					this.setState({
-						lastStrokeIdx: this.state.lastStrokeIdx + 1
-					});
-					ALL_STROKES.push(new Stroke(p5.createVector(p5.mouseX, p5.mouseY)));
-				}}
-				
-				mouseDragged = {p5 => {
-					ALL_STROKES[this.state.lastStrokeIdx].add(p5, p5.createVector(p5.mouseX, p5.mouseY));
-				}}
-				
+				setup={this.setup}
+				draw = {this.draw}
+				mousePressed = {this.mousePressed}
+				mouseDragged = {this.mouseDragged}
 				keyReleased = {p5 => {
-
-				}}/>
+				}}
+				/>
 		</div>
 	);
     }
