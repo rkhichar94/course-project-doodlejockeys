170fd2cab947ce162ebb43620f6580d901b96948
claracoo <claracoo@buffalo.edu>
2020-09-30 01:22:41 -0400

    task #31 css fixed for timer

--- doodle.bob/src/components/PlayPage.jsx
+++ doodle.bob/src/components/PlayPage.jsx
@@ -6,81 +6,44 @@ import Canvas from './Canvas';
 import Toolbar from './Toolbar';
 
 
-class Back extends Component{
+class Back extends Component {
 	render() {
 		return (
-		<div>
-			<img src={require("../images/back.png")}
-                  alt="backbutton"
-                  class="backbutton" />
+			<div>
+				<img src={require("../images/back.png")}
+					alt="backbutton"
+					class="backbutton" />
 			</div>
 		)
-}
+	}
 }
 
 function PlayPage() {
-    const gameInfo = {
-        currentGames: [
-            { gameid: 0, joincode: "0000", currentRound: 1, totalRounds: 5, currentArtistId: 2, currentWord: "ball" },
-            { gameid: 1, joincode: "1111", currentRound: 1, totalRounds: 5, currentArtistId: 2, currentWord: "ball" }
-        ],
-        users: {
-<<<<<<< HEAD
-            1: { userid: 1, username: "iguanaoverlord", profilePic: "duck", role: "guesser", preRoundScore: 0, thisRoundScore: 0 },
-            2: { userid: 2, username: "chickennuggets", profilePic: "chicken", role: "artist", preRoundScore: 0, thisRoundScore: 0 },
-            3: { userid: 3, username: "walrusparade", profilePic: "rhino", role: "guesser", preRoundScore: 0, thisRoundScore: 0 }
-=======
-<<<<<<< HEAD
-            1: { userid: 1, username: "iguanaoverlord", profilePic: "duck", role: "artist", score: 0 },
-            2: { userid: 2, username: "chickennuggets", profilePic: "chicken", role: "guesser", score: 0 },
-            3: { userid: 3, username: "walrusparade", profilePic: "rhino", role: "guesser", score: 0 }
-=======
-            1: { userid: 1, username: "iguanaoverlord", profilePic: "duck", role: "guesser", preRoundScore: 0, thisRoundScore: 0 },
-            2: { userid: 2, username: "chickennuggets", profilePic: "chicken", role: "artist", preRoundScore: 0, thisRoundScore: 0 },
-            3: { userid: 3, username: "walrusparade", profilePic: "rhino", role: "guesser", preRoundScore: 0, thisRoundScore: 0 }
->>>>>>> 69014b4... User story #8 correct guesses yield green message with word hidden, props.thisRoundScore updated only once
->>>>>>> 3c331692609c665afe60438c9ca72d3fb35341fc
-        },
+	const gameInfo = {
+		currentGames: [
+			{ gameid: 0, joincode: "0000", currentRound: 1, totalRounds: 5, currentArtistId: 2, currentWord: "ball" },
+			{ gameid: 1, joincode: "1111", currentRound: 1, totalRounds: 5, currentArtistId: 2, currentWord: "ball" }
+		],
+		users: {
+			1: { userid: 1, username: "iguanaoverlord", profilePic: "duck", role: "guesser", preRoundScore: 0, thisRoundScore: 0 },
+			2: { userid: 2, username: "chickennuggets", profilePic: "chicken", role: "artist", preRoundScore: 0, thisRoundScore: 0 },
+			3: { userid: 3, username: "walrusparade", profilePic: "rhino", role: "guesser", preRoundScore: 0, thisRoundScore: 0 }
+		},
 	};
 
- 	   return (
-		
-        <React.Fragment>
-<<<<<<< HEAD
+	return (
 
+		<React.Fragment>
 			<div className="container">
 				<div className="left-col">
 					<Scoreboard userList={gameInfo.users} />
 					<Clock />
 				</div>
-
-=======
-<<<<<<< HEAD
-            <Canvas />
-			<Scoreboard  userList={gameInfo.users}/>
-            <Chat
-                gameid={gameInfo.currentGames[0].gameid}
-                userList={gameInfo.users}
-                round={gameInfo.currentGames[0].currentRound}
-                word={gameInfo.currentGames[0].currentWord}
-            />
-            <Toolbar />
-            <Clock />
-=======
->>>>>>> 2d6498c... Task #21: Add styles to make everything fall in place.
-
-			<div className="container">
-				<div className="left-col">
-					<Scoreboard userList={gameInfo.users} />
-					<Clock />
-				</div>
-
->>>>>>> 3c331692609c665afe60438c9ca72d3fb35341fc
 				<div className="center-col">
 					<Canvas />
 					<Toolbar />
 				</div>
-				
+
 				<div className="right-col">
 					<Chat
 						gameid={gameInfo.currentGames[0].gameid}
@@ -90,8 +53,8 @@ function PlayPage() {
 					/>
 				</div>
 			</div>
-        </React.Fragment>
-    );
+		</React.Fragment>
+	);
 }
 
 export default PlayPage;
