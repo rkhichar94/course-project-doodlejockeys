22b6fb90919be0564de136d6162574b951598ef8
Arjun Bhaskar <bhaskar4@buffalo.edu>
2020-09-30 02:57:52 -0400

    Userstory #19 Implement basic timer with pause and resume feature

--- doodle.bob/src/components/Clock.jsx
+++ doodle.bob/src/components/Clock.jsx
@@ -1,47 +1,76 @@
-import React from "react";
+import React, { Component } from 'react';
+
+const TIMER_VALUE = 5;
 
 
-export default function App() {
-  const [counterSecond, setCounterSecond] = React.useState(0);
-  const [counter, setCounter] = React.useState(120);
-  const [time, setTime] = React.useState("");
-  const [status, setStatus] = React.useState("working");
+class Clock extends Component {
 
-  React.useEffect(() => {
-    let secondCounterId;
-    let counterId;
-    if (status === "working") {
-      secondCounterId = setTimeout(
-        () => setCounterSecond(counterSecond + 1),
-        1000
-      );
-      counterId = setTimeout(() => setCounter(counter - 1), 1000);
+    constructor(props) {
+      super(props);
+
+        
+      this.state = {
+        seconds: TIMER_VALUE,
+        paused: false,
+        status: "Pause"
+      };
+      this.clockToggle = this.clockToggle.bind(this);
+    }
+    
+    
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(), 1000
+        );
     }
 
-    return () => {
-      clearTimeout(counterId);
-      clearTimeout(secondCounterId);
-    };
-  }, [counterSecond, counter, status]);
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
 
-  const handletimer = () => {
-    setTime(counterSecond);
-  };
-  const stopTimers = () => {
-    setStatus("paused");
-  };
-  const resume = () => {
-    setStatus("working");
-  };
+    clockToggle() {
+      this.setState({
+        paused: !this.state.paused,
+        status: this.state.paused?"Pause":"Resume"
+      });
 
-  return (
-    <div className="App">
-      
-      <div>Countdown : {counter}</div>
-      <div> time: {time} </div>
-     
-      <button onClick={stopTimers}>Pause</button>
-      <button onClick={resume}>Resume</button>
-    </div>
-  );
+      if(this.state.status == "Reset") {
+        this.setState({
+          seconds: TIMER_VALUE,
+          paused: false,
+          status: "Pause"
+        });
+
+        this.timerId = setInterval(
+          () => this.tick(), 1000
+        );
+      }
+    }
+
+    tick() {
+      if(this.state.seconds <= 1) {
+        clearInterval(this.timerId);
+        this.setState({
+          status: "Reset"
+        })
+      }
+
+      if(this.state.paused == false) {
+        this.setState({
+            seconds: this.state.seconds - 1
+        });
+    
+      }   
+    }
+
+    render(props) {
+        return (
+        <div id="clock">
+            <p>{this.state.seconds + "s"}</p>
+            <button onClick={this.clockToggle}>{this.state.status}</button>
+        </div>
+    );
+    }
 }
+
+export default Clock;
